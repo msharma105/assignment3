36695b26f35419e9993ebb037c86b9d650e4a25c
msharma105 <39156107+msharma105@users.noreply.github.com>
2020-05-10 22:51:16 +0530

    moving to pr

--- urlRegEx.py
+++
@@ -1,9 +0,0 @@
-import re
-inputFile=open (r"C:\Users\91829\Desktop\Hiver Doc\learning\python\input_file.txt","r")
-str=inputFile.read().replace('\n', '');
-print(str)
-regex='(http[s]?://([a-zA-Z0-9]+\.)+[a-zA-Z]+(/[a-zA-Z0-9]*)+)'
-x = re.findall(regex,str,re.DOTALL)
-x = [tuple(j for j in i if j)[0] for i in x]
-print(x)
-inputFile.close()
