dfc641bd320753c4abdb05515369c5685c79eed1
msharma105 <39156107+msharma105@users.noreply.github.com>
2020-05-11 14:35:07 +0530

    Update My exception.java

--- My exception.java
+++ My exception.java
@@ -3,17 +3,13 @@ public class MyException extends Exception {
 		// Call constructor of parent Exception
 		super(s);
 	}
-
 	public static void main(String[] args) {
 		try {
-
 			throw new MyException("user defined exception");
 		} catch (MyException ex) {
-			System.out.println("Caught");
-
+			System.out.println("Catch it");
 			// Print the message from MyException object
 			System.out.println(ex.getMessage());
 		}
-
 	}
 }
